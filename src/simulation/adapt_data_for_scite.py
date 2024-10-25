6359ec7f7b953fc4bb5274a68f844392e92bee2d
Sophie <13159214+seidels@users.noreply.github.com>
2024-10-25 16:13:48 +0200

    Adapt workflow for 100 seeds per parameter setting

--- src/simulation/adapt_data_for_scite.py
+++ src/simulation/adapt_data_for_scite.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from functions import transform_array_to_presence_absence_matrix
 
-def adapt_data_for_scite_inference(read_counts_file, output_dir):
+def adapt_data_for_scite_inference(read_counts_file, output_dir, seed=1):
 
     # Ensure the output directory exists
     if not os.path.exists(output_dir):
@@ -15,7 +15,7 @@ def adapt_data_for_scite_inference(read_counts_file, output_dir):
 
     input_dat_scite = transform_array_to_presence_absence_matrix(read_counts)
 
-    output_file = os.path.join(output_dir, "scite_input.txt")
+    output_file = os.path.join(output_dir, f"scite_input_{seed}.txt")
     np.savetxt(output_file, input_dat_scite, delimiter=" ", fmt="%d")
 
 
@@ -24,6 +24,7 @@ if __name__ == "__main__":
     # Get the parameters from the command line arguments
     read_counts_file = sys.argv[1]
     output_dir =  sys.argv[2]
+    seed = sys.argv[3]
 
 
-    adapt_data_for_scite_inference(read_counts_file, output_dir)
+    adapt_data_for_scite_inference(read_counts_file, output_dir, seed)
--- src/simulation/simulate_data.py
+++ src/simulation/simulate_data.py
@@ -6,8 +6,11 @@ import numpy as np
 
 from functions import reparameterize_beta, simulate_mutation_freq_tree, simulate_read_counts
 
-def simulate_data(num_mutations, concentration, error_rate, num_reads, num_cells, output_dir):
+def simulate_data(num_mutations, concentration, error_rate, num_reads, num_cells, output_dir, seed=1):
 
+    # set seed
+    np.random.seed(seed)
+    
     # Ensure the output directory exists
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -16,14 +19,14 @@ def simulate_data(num_mutations, concentration, error_rate, num_reads, num_cells
     tree = simulate_mutation_freq_tree(num_mutations, concentration)
     
     # Write the tree to a file using networkx
-    tree_file = os.path.join(output_dir, "tree.gml")
+    tree_file = os.path.join(output_dir, f"tree_{seed}.gml")
     nx.write_gml(tree, tree_file)
     
     # Simulate the read counts
     read_counts = simulate_read_counts(tree, error_rate, num_reads, num_cells)
     
     # Write the read counts to a text file using numpy.savetxt for efficiency
-    read_counts_file = os.path.join(output_dir, "read_counts")
+    read_counts_file = os.path.join(output_dir, f"read_counts_{seed}.npy")
     np.save(read_counts_file, read_counts)
 
 
@@ -36,7 +39,8 @@ if __name__ == "__main__":
     num_reads = int(sys.argv[4])
     num_cells = int(sys.argv[5])
     output_dir = sys.argv[6]
+    seed=int(sys.argv[7])
 
     
     # Run the simulation
-    simulate_data(num_mutations, concentration, error_rate, num_reads, num_cells, output_dir)
+    simulate_data(num_mutations, concentration, error_rate, num_reads, num_cells, output_dir, seed)
